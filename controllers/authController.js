104fdebf79b8266cb2457279a2038ba42dd99439
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-01-17 13:33:56 +0545

    added forgot password route

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,7 +62,7 @@ const register = async(req,res) => {
         });
 
     }catch(error){
-        res.status(500).json({
+        return res.status(500).json({
             message : "Error while Registering User",
             error : error.message
         });
@@ -128,7 +128,7 @@ const login = async(req,res) => {
         });
 
     }catch(error){
-        res.status(500).json({
+        return res.status(500).json({
             message : "Error while Logging",
             error : error.message
         });
@@ -188,13 +188,71 @@ const verifyEmail = async(req,res) => {
         });
 
     }catch(error){
-        res.status(500).json({
+        return res.status(500).json({
             message : "Error while Verifying Email",
             error : error.message
         });
     }
 }
 
+
+//forgect password
+const forgotPassword = async(req, res) =>{
+    try{
+        const {email} = req.body;
+
+        if(!email){
+            return res.status(400).json({
+                message : "pleasee enter valid email address"
+            })
+        }
+
+        const user = await User.findOne({where:{email: email}});
+        if(!user){
+            return res.status(400).json({
+                message : "unregistered email"
+            })
+        }
+        
+        
+        //Verification Token
+        const verificationToken = await crypto.randomBytes(32).toString('hex');
+
+        const verificationExpiresIn = new Date(Date.now() + 1 * 60 * 60 * 1000);
+
+        //link inside email
+        const verifyLink = `http://localhost:3000/api/user/reset-password?token=${verificationToken}`;
+
+        const html = verificationEmailTemplate("gautam",verifyLink);
+
+        //Send Email  to the user email to verify 
+        const isEmailSent = sendEmail(email,"Verification Email",html);
+
+        if(!isEmailSent){
+            return res.status(400).json({
+                message : "Error while sending Verification Code"
+            });
+        }
+        
+
+        user.verificationToken = verificationToken,
+        user.verificationTokenExpires = verificationExpiresIn,
+
+        await user.save();
+
+        return res.status(201).json({
+            message: "email sent, please verify yourself"
+        })
+
+    
+    }catch(error){
+        return res.status(400).json({
+            message: "something went wrong",
+            error: error.message
+        })
+    }
+}
+
 module.exports = {
-    register,login,logout,verifyEmail
+    register,login,logout,verifyEmail, forgotPassword
 }
